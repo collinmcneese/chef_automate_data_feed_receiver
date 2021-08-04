55fe5065aa6d523e489e93c1cef4d528129de503
Collin McNeese <cmcneese@chef.io>
2021-08-03 20:11:31 -0500

    sqlite updates
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- sqlite/controllers/DataFeedController.js
+++ sqlite/controllers/DataFeedController.js
@@ -5,24 +5,86 @@ const Op = Sequelize.Op;
 const data_feed_infra = db.models.df_infra_data_feed;
 const data_feed_compliance = db.models.df_compliance_data_feed;
 
+function countInArray(array, value) {
+  return array.reduce((n, x) => n + (x === value), 0);
+}
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
 exports.getAllInfraData = async() => {
   try {
     var reply = await data_feed_infra.findAll({});
-    // var response = [];
-    // for (var object of reply) {
-    //   var objectClientRun = strip_json_string(object.client_run);
-    //   var objectAttributes = JSON.parse(strip_json_string(object.attributes));
-    //   var respObject = {
-    //     node_id: reply.node_id,
-    //     client_run: JSON.parse(objectClientRun),
-    //     normal_attributes: objectAttributes.normal,
-    //     default_attributes: objectAttributes.default,
-    //     automatic_attributes: objectAttributes.automatic,
-    //     created: object.createdAt,
-    //     updated: object.updatedAt,
-    //   };
-    //   response.push(respObject);
-    // };
+    return reply;
+  } catch (err) {
+    console.log(err);
+    throw boom.boomify(err);
+  }
+};
+
+exports.getInfraNodeRun = async(req) => {
+  try {
+    var reply = await data_feed_infra.findAll({
+      where: {
+        name: req.params.name,
+      },
+    });
+    return reply;
+  } catch (err) {
+    console.log(err);
+    throw boom.boomify(err);
+  }
+};
+
+exports.getInfraNode = async(req) => {
+  try {
+    var reply = await data_feed_infra.findAll({
+      where: {
+        name: req.params.name,
+      },
+    });
+    return reply;
+  } catch (err) {
+    console.log(err);
+    throw boom.boomify(err);
+  }
+};
+
+exports.getInfraNodeList = async() => {
+  try {
+    var reply = await data_feed_infra.findAll({
+      attributes: [
+        'node_id',
+        'platform',
+        'name',
+        'createdAt',
+        'updatedAt',
+      ],
+    });
+    return reply;
+  } catch (err) {
+    console.log(err);
+    throw boom.boomify(err);
+  }
+};
+
+exports.getInfraNodeListDetails = async(req) => {
+  try {
+    var reply = await data_feed_infra.findAll({
+      where: {
+        name: {
+          [Op.in]: req.payload.node_list,
+        },
+      },
+      attributes: [
+        'node_id',
+        'client_run',
+        'platform',
+        'name',
+        'createdAt',
+        'updatedAt',
+      ],
+    });
     return reply;
   } catch (err) {
     console.log(err);
@@ -40,78 +102,12 @@ exports.getAllComplianceData = async() => {
   }
 };
 
-exports.getNodeRun = async(req) => {
-  try {
-    // var rName = req.params.name;
-    var reply = await data_feed_infra.findAll({
-      where: {
-        name: req.params.name.toLowerCase(),
-      },
-    });
-    return reply;
-  } catch (err) {
-    console.log(err);
-    throw boom.boomify(err);
-  }
-};
-
-exports.getNode = async(req) => {
-  try {
-    var reply = await data_feed_infra.findAll({
-      where: {
-        name: req.params.name.toLowerCase(),
-      },
-    });
-    return reply;
-  } catch (err) {
-    console.log(err);
-    throw boom.boomify(err);
-  }
-};
-
-exports.getNodes = async() => {
-  try {
-    var reply = await data_feed_infra.findAll({
-      attributes: [
-        'node_id',
-        // 'client_run',
-        'platform',
-        'name',
-        'createdAt',
-        'updatedAt',
-      ],
-    });
-    // console.log(Object.keys(reply[0].dataValues))
-    return reply;
-  } catch (err) {
-    console.log(err);
-    throw boom.boomify(err);
-  }
-};
-
-exports.getObjectKeys = async(req) => {
-  try {
-    var rName = req.params.name;
-    var reply = await data_feed_infra.findOne({
-      where: {
-        name: {
-          [Op.like]: rName,
-        },
-      },
-    });
-    console.log(Object.keys(reply.dataValues));
-    return Object.keys(reply.dataValues);
-  } catch (err) {
-    console.log(err);
-    throw boom.boomify(err);
-  }
-};
-
 exports.getProfileList = async(req) => {
   try {
     var profile_list_raw = await data_feed_compliance.findAll({
       attributes: [
         'profile_name',
+        'profile_full',
       ],
       raw: true,
     });
@@ -121,14 +117,8 @@ exports.getProfileList = async(req) => {
       profile_list.push(profile.profile_name);
     }
     console.log(profile_list);
-    function countInArray(array, value) {
-      return array.reduce((n, x) => n + (x === value), 0);
-    }
-    function onlyUnique(value, index, self) {
-      return self.indexOf(value) === index;
-    }
-    for (var profile of profile_list.filter(onlyUnique)) {
-      reply[profile] = countInArray(profile_list, profile);
+    for (var p of profile_list.filter(onlyUnique)) {
+      reply[p] = countInArray(profile_list, p);
     }
     return reply;
   } catch (err) {
@@ -141,7 +131,7 @@ exports.getProfileListByNode = async(req) => {
   try {
     var reply = await data_feed_compliance.findAll({
       where: {
-        name: req.params.name.toLowerCase(),
+        name: req.params.name,
       },
       attributes: [
         'node_id',
@@ -154,7 +144,6 @@ exports.getProfileListByNode = async(req) => {
         'updatedAt',
       ],
     });
-    // console.log(Object.keys(reply[0].dataValues))
     return reply;
   } catch (err) {
     console.log(err);
@@ -162,11 +151,30 @@ exports.getProfileListByNode = async(req) => {
   }
 };
 
-exports.getProfileDetailsByNode = async(req) => {
+exports.getComplianceNodeList = async(req) => {
+  try {
+    var node_list_raw = await data_feed_compliance.findAll({
+      attributes: [
+        'name',
+      ],
+      raw: true,
+    });
+    var node_list = [];
+    for (var n of node_list_raw) {
+      node_list.push(n.name);
+    }
+    return node_list;
+  } catch (err) {
+    console.log(err);
+    throw boom.boomify(err);
+  }
+};
+
+exports.getComplianceDetailsByNode = async(req) => {
   try {
     var reply = await data_feed_compliance.findAll({
       where: {
-        name: req.params.name.toLowerCase(),
+        name: req.params.name,
       },
       attributes: [
         'node_id',
@@ -187,22 +195,25 @@ exports.getProfileDetailsByNode = async(req) => {
   }
 };
 
-exports.getNodesByPlatform = async(req) => {
+exports.getComplianceDetailsByNodeList = async(req) => {
   try {
-    var reply = await data_feed_infra.findAll({
-      attributes: [
-        'node_id',
-        'name',
-        'platform',
-        ['createdAt', 'created'],
-        ['updatedAt', 'updated'],
-      ],
+    var reply = await data_feed_compliance.findAll({
       where: {
-        platform: {
-          [Op.like]: req.params.platform,
+        name: {
+          [Op.in]: req.payload.node_list,
         },
       },
-      raw: true,
+      attributes: [
+        'node_id',
+        'platform',
+        'name',
+        'profile_name',
+        'profile_full',
+        'profile_status',
+        'profile_controls',
+        'createdAt',
+        'updatedAt',
+      ],
     });
     return reply;
   } catch (err) {
@@ -211,6 +222,7 @@ exports.getNodesByPlatform = async(req) => {
   }
 };
 
+// Data Feed Base Functions
 exports.addData = async(req) => {
   try {
     if (req.payload.attributes) {
@@ -250,16 +262,14 @@ exports.addData = async(req) => {
       }
     } else if (req.payload.report) {
       console.log('Compliance-only report sent');
-      console.log(req.payload);
-      // console.log(req.payload.report.profiles);
       for (var profile of req.payload.report.profiles) {
         var rPlatform_name = req.payload.report.platform.name;
-        var rNode_name = req.payload.report.node_name.toLowerCase();
+        var rNode_name = req.payload.report.node_name;
         var rProfile_name = profile.name;
         var rProfile_name_full = profile.full;
         var rProfile_controls = profile.controls || [];
         var rProfile_status = profile.status;
-        var reply = await data_feed_compliance.findOrCreate({
+        var reply_compliance = await data_feed_compliance.findOrCreate({
           where: {
             node_id: req.payload.report.node_id,
             profile_name: profile.name,
@@ -274,7 +284,7 @@ exports.addData = async(req) => {
           },
         });
         // If record was already present (false), run update action
-        if (reply[1] === false) {
+        if (reply_compliance[1] === false) {
           await data_feed_compliance.update({
             profile_full: rProfile_name_full,
             profile_status: rProfile_status,
--- sqlite/routes/data_feed.js
+++ sqlite/routes/data_feed.js
@@ -2,40 +2,45 @@ const DataFeedController = require('../controllers/DataFeedController');
 const Joi = require('joi');
 
 const routes = [
+  // Data Feed base routes
+  {
+    method: 'POST',
+    path: '/datafeed/add',
+    handler: DataFeedController.addData,
+    options: {
+      auth: false,
+      description: 'Add new data.',
+      tags: ['api', 'datafeed'],
+      validate: {
+        payload: Joi.object(),
+      },
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/datafeed/purge',
+    handler: DataFeedController.delNodeData,
+    options: {
+      auth: false,
+      description: 'Delete all node data',
+      tags: ['api', 'datafeed'],
+    },
+  },
+  // Infra routes
   {
     method: 'GET',
-    path: '/datafeed/list/infra/all',
+    path: '/datafeed/list/infra/alldata',
     handler: DataFeedController.getAllInfraData,
     options: {
       auth: false,
-      description: 'Get all data',
+      description: 'Get all Infra data (demo only, should not be used in live data set)',
       tags: ['api', 'infra'],
     },
   },
   {
     method: 'GET',
-    path: '/datafeed/list/compliance/all',
-    handler: DataFeedController.getAllComplianceData,
-    options: {
-      auth: false,
-      description: 'Get all data',
-      tags: ['api', 'compliance'],
-    },
-  },
-  {
-    method: 'GET',
-    path: '/datafeed/list/nodes',
-    handler: DataFeedController.getNodes,
-    options: {
-      auth: false,
-      description: 'Get node data for all nodes.',
-      tags: ['api', 'infra'],
-    },
-  },
-  {
-    method: 'GET',
-    path: '/datafeed/list/node/{name}',
-    handler: DataFeedController.getNode,
+    path: '/datafeed/list/infra/node/{name}',
+    handler: DataFeedController.getInfraNode,
     options: {
       auth: false,
       description: 'Get all Infra data for a node by name',
@@ -45,6 +50,55 @@ const routes = [
           name: Joi.string().min(1).required(),
         }),
       },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/datafeed/list/infra/nodes',
+    handler: DataFeedController.getInfraNodeList,
+    options: {
+      auth: false,
+      description: 'Get listing of all Infra nodes.',
+      tags: ['api', 'infra'],
+    },
+  },
+  {
+    method: 'POST',
+    path: '/datafeed/list/infra/nodes',
+    handler: DataFeedController.getInfraNodeListDetails,
+    options: {
+      auth: false,
+      description: 'Get all Infra data for a list of nodes by name',
+      tags: ['api', 'infra'],
+      validate: {
+        payload: Joi.object({
+          node_list: Joi.array().items(Joi.string().min(1).required()),
+        }),
+      },
+    },
+  },
+  {
+    method: 'POST',
+    path: '/datafeed/search/infra/attributes',
+    handler: () => { return 'Not yet implemented.'; },
+    options: {
+      auth: false,
+      description: 'Search Infra attribute data',
+      tags: ['api', 'datafeed'],
+      validate: {
+        payload: Joi.object(),
+      },
+    },
+  },
+  // Compliance routes
+  {
+    method: 'GET',
+    path: '/datafeed/list/compliance/alldata',
+    handler: DataFeedController.getAllComplianceData,
+    options: {
+      auth: false,
+      description: 'Get all Compliance data (demo only, should not be used in live data set)',
+      tags: ['api', 'compliance'],
     },
   },
   {
@@ -74,8 +128,33 @@ const routes = [
   },
   {
     method: 'GET',
-    path: '/datafeed/details/compliance/profiles/node/{name}',
-    handler: DataFeedController.getProfileDetailsByNode,
+    path: '/datafeed/list/compliance/nodes',
+    handler: DataFeedController.getComplianceNodeList,
+    options: {
+      auth: false,
+      description: 'Get listing of all Compliance nodes.',
+      tags: ['api', 'compliance'],
+    },
+  },
+  {
+    method: 'POST',
+    path: '/datafeed/detail/compliance/nodes',
+    handler: DataFeedController.getComplianceDetailsByNodeList,
+    options: {
+      auth: false,
+      description: 'Get detailed Compliance data for a list of nodes by name',
+      tags: ['api', 'compliance'],
+      validate: {
+        payload: Joi.object({
+          node_list: Joi.array().items(Joi.string().min(1).required()),
+        }),
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/datafeed/detail/compliance/node/{name}',
+    handler: DataFeedController.getComplianceDetailsByNode,
     options: {
       auth: false,
       description: 'Get detailed Compliance data for a node by name, including Control data',
@@ -85,59 +164,6 @@ const routes = [
           name: Joi.string().min(1).required(),
         }),
       },
-    },
-  },
-  {
-    method: 'GET',
-    path: '/datafeed/list/keys/nodes/{name}',
-    handler: DataFeedController.getObjectKeys,
-    options: {
-      auth: false,
-      description: 'Get object keys based on a provided Node name.',
-      tags: ['api', 'infra'],
-      validate: {
-        params: Joi.object({
-          name: Joi.string().min(1).required(),
-        }),
-      },
-    },
-  },
-  {
-    method: 'GET',
-    path: '/datafeed/list/nodes/platform/{platform}',
-    handler: DataFeedController.getNodesByPlatform,
-    options: {
-      auth: false,
-      description: 'Get nodes by platform',
-      tags: ['api', 'infra'],
-      validate: {
-        params: Joi.object({
-          platform: Joi.string().min(1).required(),
-        }),
-      },
-    },
-  },
-  {
-    method: 'POST',
-    path: '/datafeed/add',
-    handler: DataFeedController.addData,
-    options: {
-      auth: false,
-      description: 'Add new data.',
-      tags: ['api', 'datafeed'],
-      validate: {
-        payload: Joi.object(),
-      },
-    },
-  },
-  {
-    method: 'DELETE',
-    path: '/datafeed/purge',
-    handler: DataFeedController.delNodeData,
-    options: {
-      auth: false,
-      description: 'Delete all node data',
-      tags: ['api', 'datafeed'],
     },
   },
 ];
