bd4aa1fb4f3c28e7c935d1a7cc7b73a2440a3e52
Collin McNeese <collin@mydevsandbox.com>
2022-05-22 08:11:35 -0500

    License Update

--- mssql/habitat/plan.sh
+++ mssql/habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=automate-data-feed-receiver-mssql
 pkg_origin=collinmcneese
 pkg_version="0.1.0"
-pkg_maintainer="Collin McNeese <cmcneese@chef.io>"
+pkg_maintainer="Collin McNeese <collin@mydevsandbox.com>"
 pkg_license=("Apache-2.0")
 pkg_scaffolding="core/scaffolding-node"
 
